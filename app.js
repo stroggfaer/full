0c95ac2d393d55211dcaf2b904f494533cb6df54
ruslan <rendzhi@mail.ru>
2019-09-30 15:52:11 +0700

    test

--- app.js
+++ app.js
@@ -13,17 +13,6 @@ db.authenticate().then(() => {
         console.error('Unable to connect to the database:', err);
     });
 
-/*
-  app.use(passport.initialize())
-  require('./middleware/passport')(passport)
-
-  app.use(require('morgan')('dev'))
-  app.use('/uploads', express.static('uploads'))
-*/
-// app.use(bodyParser.urlencoded({extended: true}))
-// app.use(bodyParser.json())
-//
-
 // configure the app to use bodyParser()
 app.use(bodyParser.urlencoded({
     extended: true
@@ -31,12 +20,6 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use(require('cors')())
-
-// app.use('/api/auth', authRoutes)
-// app.use('/api/analytics', analyticsRoutes)
-// app.use('/api/category', categoryRoutes)
-// app.use('/api/order', orderRoutes)
-// app.use('/api/position', positionRoutes)
 
 app.use('/api/department', departmentRoutes)
 
